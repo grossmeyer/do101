c0bc95f4a0eff5734cb0ae87dcf3524ffd43ad55
Glenn Meyer <grossmeyer@gmail.com>
2020-04-18 11:44:47 -0400

    added / and /api

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,14 @@
-//TODO add a comment
-console.log("Hello DO101!\n");
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello DO101!\n')
+});
+
+app.get('/api', function (req, res) {
+    res.send('Hi, this is a test API!\n')
+});
+
+app.listen(8080, function () {
+    console.log('Example web server listening on port 8080...\n');
+});
